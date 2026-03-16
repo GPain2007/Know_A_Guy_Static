7e2ecb7b7baaf238cef870881d311a2b485f9d31
GenThrive <genthrive@ecorise.org>
2026-03-16 15:48:03 -0500

    shuffal around

--- src/App.js
+++ src/App.js
@@ -7,14 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Oh I Know A Guy Website in the works! <br />
+          <span className="App-company">Oh..I KNOW A GUY! LLC</span> <br />{" "}
+          Website in the works! <br />
           For a free estimate contact us at: <br />
           (737)259-5124 or{" "}
           <a href="mailto:I.knowaguy.lawn1@gmail.com">
             I.knowaguy.lawn1@gmail.com
           </a>{" "}
-          <br />
-          <span className="App-company">Oh..I KNOW A GUY! LLC</span>
         </p>
       </header>
     </div>
