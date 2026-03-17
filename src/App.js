ba3099a36109fb342fbe7d86461719d41b629218
GenThrive <genthrive@ecorise.org>
2026-03-17 14:50:57 -0500

    made file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from "./TrimLogo.png";
-import "./App.css";
+import { Header } from "./components/Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <span className="App-company">Oh..I KNOW A GUY! LLC</span> <br />{" "}
-          Website in the works! <br />
-          For a free estimate contact us at: <br />
-          (737)259-5124 or{" "}
-          <a href="mailto:I.knowaguy.lawn1@gmail.com">
-            I.knowaguy.lawn1@gmail.com
-          </a>{" "}
-        </p>
-      </header>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import logo from "../../TrimLogo.png";
+import "../../App.css";
+
+export const Header = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <span className="App-company">Oh..I KNOW A GUY! LLC</span> <br />{" "}
+          Website in the works! <br />
+          For a free estimate contact us at: <br />
+          (737)259-5124 or{" "}
+          <a href="mailto:I.knowaguy.lawn1@gmail.com">
+            I.knowaguy.lawn1@gmail.com
+          </a>{" "}
+        </p>
+      </header>
+    </div>
+  );
+};
